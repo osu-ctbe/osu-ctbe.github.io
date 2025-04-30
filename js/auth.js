5db312e3a9db4265685ed1201f8b963a957884ab
Randelman4ik <kirik-agent@yandex.ru>
2025-04-30 22:29:26 +0300

    test2

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Парсим access_token из URL (например: https://.../callback.html#access_token=XXX)
-    const hash = window.location.hash.substring(1);
+    const hash = window.location.hash.substring(1); // Удаляем #
     const params = new URLSearchParams(hash);
     const accessToken = params.get('access_token');
-
+    
     if (accessToken) {
         localStorage.setItem('osu_access_token', accessToken);
         window.location.href = 'profile.html';
     } else {
-        alert('Ошибка авторизации!');
+        alert('Ошибка: токен не получен!');
         window.location.href = 'index.html';
     }
 });
